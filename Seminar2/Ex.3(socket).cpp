7c6ef3487ab15f774b85d63e1306c82c81bd3e5a
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2021-03-31 01:03:05 +0300

    Update the third exercise

--- Seminar2/Ex.3(socket).cpp
+++ Seminar2/Ex.3(socket).cpp
@@ -1,42 +1,41 @@
-class ConnectionFabric {
-public:
-	static socket* open_connection(); //Создает объект типа socket и возвращает указатель на него
-
-	static socket* clone_connection(socket* ptr); //Копирует уже существующий объект типа socket и возвращает указатель на копию
-
-	static void close_connection(socket* ptr); //Удаляет объект типа socket по его адресу. 
-};
-
 class connection_storage {
 public:
-	connection_storage() : target(nullptr) {} // конструктор по умолчанию ничего не делает
-
+	connection_storage() : target(ConnectionFabric::open_connection()) {} // конструктор по умолчанию ничего не делает
+        ~connection_storage()
+        {
+            if (target != nullptr)
+            {
+                ConnectionFabric::close_connection(target);
+            }
+             target = nullptr;
+        }
 	connection_storage(connection_storage const& rhs); //конструктор копирования
 	connection_storage(connection_storage&& rhs); //конструктор перемещения
 
 	socket* connection(); //возвращает указатель на socket
 	socket const* connection() const; //аналогично выше, но с const 
 private:
-	const socket* target;
+	socket* target;
 };
 
-connection_storage::connection_storage(connection_storage const& rhs) : target(ConnectionFabric::clone_connection(rhs.connection()))
+connection_storage::connection_storage(connection_storage const& rhs) : target(ConnectionFabric::clone_connection(const_cast<socket*>(rhs.connection())))
 {
 
 }
 
 connection_storage::connection_storage(connection_storage&& rhs) : target(rhs.connection())
 {
-	ConnectionFabric::close_connection(rhs.connection());
-	rhs.~connection_storage();
+	//ConnectionFabric::close_connection(rhs.connection());
+	//rhs.target = nullptr;
 }
 
 socket* connection_storage::connection()
 {
-	return const_cast<socket*>(target);
+	return target;
 }
 
 socket const* connection_storage::connection() const
 {
-	return target;
+        const socket* CT = target;
+	return CT;
 }
